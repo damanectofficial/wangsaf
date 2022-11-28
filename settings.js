09e7af53578eda7e490752d46e9b6dea58833435
damanectofficial <88991901+damanectofficial@users.noreply.github.com>
2022-11-28 14:37:27 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -23,27 +23,19 @@
 
 const fs = require('fs')
 const chalk = require('chalk')
-
-//Api Website\\
+// Website Api
 global.APIs = {
 	zenz: 'https://zenzapis.xyz',
 }
 
-//Api Key\\
+// Apikey Website Api
 global.APIKeys = {
-	'https://zenzapis.xyz': 'your key',
+	'https://zenzapis.xyz': 'Your Key',
 }
-
 
 //aumto functioner
 global.autoTyping = false //auto tying in gc (true to on, false to off)
-global.autoreadpmngc = true //auto reading in gc and pm (true to on, false to off)
-global.autoReadGc = true //auto reading in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
-global.available = true //auto available (true to on, false to off)
-
-//semt menu variants
-global.vidmenu = fs.readFileSync("./XeonMedia/theme/Cheems-bot.mp4") //gif and video menu
 
 //documents variants
 global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
@@ -56,26 +48,56 @@ global.doc6 = 'application/vnd.android.package-archive'
 //owmner v card
 global.vcardowner = ['6283872273467'] //ur owner number
 global.ownername = "🦄Hidayat Store My Id" //ur owner name
-global.ytname = "YT:" //ur yt chanel name
-global.socialm = "GitHub:" //ur github or insta name
+global.ytname = "YT: Hidayat Store" //ur yt chanel name
+global.socialm = "GitHub: damanectofficial" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['6283872273467']
+global.owner = ['6283872273467'] //ur number
+global.ownernomer = "6283872273467" //ur number
 global.ownertag = '6283872273467' //ur tag number
-global.botname = 'Hi DayatGanteng-BotWhatsap' //ur bot name
+global.premium = ['6283872273467'] //ur premium number
+global.botname = 'Hi Dayat Ganteng-Bot Whatsap' //ur bot name
+global.ownername = "🦄Hidayat Store My Id"
 global.linkz = "https://hidayat-store.my.id" //your theme url which will be displayed on whatsapp
 global.websitex = "https://hidayat-store.my.id" //ur website to be displayed
-global.botscript = 'https://hidayat-store.my.id' //script link
+global.botscript = 'https://chat.whatsapp.com/DbQIteeWjT0ICNcvNr4OOI' //script link
 global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Hi DayatGanteng-BotWhatsap" //ur sticker watermark packname
-global.author = "🦄Hidayat Store My Id" //ur sticker watermark author
+global.packname = "Sticker By" //ur sticker watermark packname
+global.author = "Hi Dayat Ganteng-Bot Whatsap" //ur sticker watermark author
+global.wm = "Hidayat Store My Id" //ur watermark
 
-//Bot theme media
+// Other
+global.sessionName = 'session'
+global.prefa = ['','!','.','#','&']
+global.sp = ''
+global.mess = {
+    success: 'Done Ngab',
+    admin: 'Fitur ini hanya dapat digunakan oleh admin!',
+    botAdmin: 'Bot Harus Jadi Admin Dulu!',
+    premime: 'Fitur Khusus Premium Jika Ingin Mendaftar bisa hubungi Owner',
+    owner: 'Fitur ini hanya dapat digunakan oleh owner',
+    group: 'Fitur Hanya Digunakan Untuk Grup!',
+    private: 'Fitur Yang Digunakan Hanya Untuk Obrolan Pribadi!',
+    bot: 'Fitur ini hanya dapat digunakan oleh bot',
+    wait: 'Sabar lagi proses:v...',
+    linkm: 'Mana link nya woi?',
+    endLimit: 'Batas Harian Anda Telah Habis, Batas Akan Direset Setiap 12 Jam',
+    nsfw: 'Fitur nsfw belum diaktifkan, silahkan hubungi admin untuk aktivasi',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 25
+}
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 global.log0 = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur logo pic
 global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
 
 //damtabase
 global.premium = ['6283872273467'] //ur premium numbers
@@ -98,49 +120,3 @@ global.gcrevoke = []
 global.autorep = []
 global.ntilink = []
 
-//omther
-global.sessionName = 'session'
-global.antitags = true
-global.prefa = ['','!','.','🐦','🐤','🗿']
-global.sp = '⭔'
-global.mess = {
-    success: 'Done ✓',
-    admin: 'This Feature Is Only For Admin!',
-    botAdmin: 'Bot Must Be Admin First!',
-    owner: 'This Feature Is Only For Owner!',
-    group: 'Feature Used Only For Groups!',
-    private: 'Features Used Only For Private Chat!',
-    bot: 'This Feature Is Only For Bot',
-    wait: 'In process...',
-    linkm: 'Where is the link?',
-    error: 'Error!!',
-    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
-    ban: 'You have been banned by the owner, if you want to be unbanned, chat owner.',
-    nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
-    banChat: 'The bot was banned in this group, please contact the owner to unban'
-}
-    global.limitawal = {
-    premium: "Infinity",
-    free: 12,
-    monayawal: 1000
-}
-   global.rpg = {
-   darahawal: 100,
-   besiawal: 15,
-   goldawal: 10,
-   emeraldawal: 5,
-   umpanawal: 5,
-   potionawal: 1
-}
-global.thumb = fs.readFileSync('./XeonMedia/theme/cheemspic.jpg')
-global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-let file = require.resolve(__filename)
-fs.watchFile(file, () => {
-	fs.unwatchFile(file)
-	console.log(chalk.redBright(`Update'${__filename}'`))
-	delete require.cache[file]
-	require(file)
-})
